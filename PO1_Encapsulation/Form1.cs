38280d595a196bb5c8e354ebdd862da7737136ab
Philippe-Morin-Veer <157501225+Philippe-Morin-Veer@users.noreply.github.com>
2024-02-08 14:26:03 -0500

    fin du premier cour

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,61 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage();
+        Personnage dragon = new();
         public Form1()
         {
             InitializeComponent();
+            joueur.PointsDeVie = 10;
+            dragon.PointsDeVie = joueur.PointsDeVie * 3;
+            joueur.PointsDePuissance = 1;
+            dragon.PointsDePuissance = joueur.PointsDePuissance * 3;
+            joueur.PointsDeVieInitial = joueur.PointsDeVie;
+            dragon.PointsDeVieInitial = dragon.PointsDeVie;
+
+        }
+
+        private void ActualiserLabel()
+        {
+            lbl_pointsVieJoueur.Text="Points de vie : "+ joueur.PointsDeVie.ToString();
+            lbl_puissanceAttaqueJoueur.Text = "Points de puissance : " + joueur.PointsDePuissance.ToString();
+            lbl_pointsVieDragon.Text = "Points de vie : " + dragon.PointsDeVie.ToString();
+            lbl_puissanceAttaqueDragon.Text="points de puissance : "+ dragon.PointsDePuissance.ToString();
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
+            dragon.PointsDeVie -= joueur.PointsDePuissance;
+            TourDragon();
+            ActualiserLabel();
 
-        }
+ }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            joueur.PointsDeVie += 5;
+            TourDragon();
+            ActualiserLabel();
+          
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            joueur.PointsDePuissance += 1;
+            TourDragon();
+            ActualiserLabel();
 
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabel();
+        }
+
+        private void TourDragon()
+        {
+            joueur.PointsDeVie -= dragon.PointsDePuissance;
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        private string nom;
+        private int pointsDeVie;
+        private int pointsDePuissance;
+        private int pointsDeVieInitial;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDeVie { get => pointsDeVie;
+            set
+            {
+               if (value > 0)
+                {
+                    pointsDeVie = value;
+                }
+                else
+                {
+                    pointsDeVie = 0;
+                }
+            }
+                }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie
+        {
+            get
+             {
+                if(pointsDeVie > 0)
+                {
+                 return true;
+                }
+                 return false;
+             }
+        }
+
+        public int PointsDeVieInitial { get => pointsDeVieInitial;
+            set
+            {
+               
+            }
+                
+                }
+    }
+}
